9c094f99e42090770a0c75c2e434dec2bb7b9b8c
Mohit <157598261+LuckyMo-it@users.noreply.github.com>
2025-06-23 11:04:02 +0530

    feat:beautify

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,18 @@
 
+import { useState } from 'react'
 import { Header } from './components/Header'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-
 function App() {
+  const [selectedHeader,setSelectedHeader]=useState('Home')
 
   return (
     <BrowserRouter>
 
-    <div className='bg-gray-200 h min-w-screen '>
+    <div className='bg-gray-200  min-w-screen '>
       <Routes>
         
       </Routes>
-      <Header/>
+      <Header selectedHeader={selectedHeader} setSelectedHeader={setSelectedHeader}/>
     </div>
     </BrowserRouter>
   )
--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,13 +1,17 @@
 import {useNavigate} from 'react-router-dom'
-export function Header(){
+export function Header({selectedHeader,setSelectedHeader}){
     const header=["Home","Post","Profile"]
     const navigate=useNavigate()
     return (
-        <div className="flex justify-between px-8  items-center  w-full h-20 fixed top-0 left-0 bg-green-400">
+        <div className="flex justify-between px-8  items-center  w-full h-20 fixed top-0 left-0  bg-gradient-to-r from-pink-500 gradiant  to-blue-300">
+            <div className="menu flex-1/6"><i className="fa-solid  fa-bars text-3xl hover:text-blue-800"></i></div>
+            <div className="tab flex-5/6  p-2">
 
             {header.map((ele,key)=>{
-                return <h3 onClick={()=>navigate(`/${ele.toLowerCase()}`)} key={key} className="text-3xl font-s w-fit transition transition-all h-full flex items-center duration-200 border-transparent hover:text-4xl hover:border-b-2 hover:border-blue-600 text-amber-50">{ele}</h3>
+                return <h3 onClick={()=>{navigate(`/${ele.toLowerCase()}`);setSelectedHeader(ele)}
+            } key={key} className={`text-3xl inline-block mx-2 border-b-2  w-fit transition transition-all h-full bg-gradient-to-l from-blue-500 to-cyan-400 text-transparent bg-clip-text  duration-200   hover:border-b-2 hover:border-blue-600 ${(selectedHeader==ele?' border-blue-600':'border-transparent')}`}>{ele}</h3>
             })}
+            </div>
         </div>
     )
 }
