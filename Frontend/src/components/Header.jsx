2220a98e8b3c6355159377c155c4c4f318b6a60e
Mohit <157598261+LuckyMo-it@users.noreply.github.com>
2025-06-23 16:36:13 +0530

    no comment

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -4,12 +4,12 @@ export function Header({selectedHeader,setSelectedHeader}){
     const navigate=useNavigate()
     return (
         <div className="flex justify-between px-8  items-center  w-full h-20 fixed top-0 left-0  bg-gradient-to-r from-pink-500 gradiant  to-blue-300">
-            <div className="menu flex-1/6"><i className="fa-solid  fa-bars text-3xl hover:text-blue-800"></i></div>
+            <div className="menu flex-1/6"><i className="fa-solid cursor-pointer fa-bars text-3xl hover:text-blue-800"></i></div>
             <div className="tab flex-5/6  p-2">
 
             {header.map((ele,key)=>{
                 return <h3 onClick={()=>{navigate(`/${ele.toLowerCase()}`);setSelectedHeader(ele)}
-            } key={key} className={`text-3xl inline-block mx-2 border-b-2  w-fit transition transition-all h-full bg-gradient-to-l from-blue-500 to-cyan-400 text-transparent bg-clip-text  duration-200   hover:border-b-2 hover:border-blue-600 ${(selectedHeader==ele?' border-blue-600':'border-transparent')}`}>{ele}</h3>
+            } key={key} className={`text-3xl cursor-pointer inline-block mx-2 border-b-2  w-fit transition transition-all h-full bg-gradient-to-l from-blue-500 to-cyan-400 text-transparent bg-clip-text  duration-200   hover:border-b-2 hover:border-blue-600 ${(selectedHeader==ele?' border-blue-600':'border-transparent')}`}>{ele}</h3>
             })}
             </div>
         </div>
